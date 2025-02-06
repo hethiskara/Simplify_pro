2a3a1a21a78c0b27b7ba6e328a53d7c4174bcb79
Hethiskarna Ravi <hethiskarnaravi@Hethiskarnas-MacBook-Air.local>
2025-02-06 13:16:36 -0800

    auth bug fix

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://simplifypro.palsbase.online";
+const API_BASE_URL = "https://simplifypro.palsbase.online";
 
 
 export const loginUser = async (email, password) => {
